fb93d88e4d1714df063e8eece01d5375c06c6461
Mateuslima18 <mateus.macedo@academico.ifpb.edu.br>
2024-11-05 17:15:46 +0000

    calculadorajs

---
+++ calculadora.js
@@ -0,0 +1,22 @@
+function appendToDisplay(value) {  
+    document.getElementById('display').value += value;  
+}  
+
+function clearDisplay() {  
+    document.getElementById('display').value = '';  
+}  
+
+function calculateResult() {  
+    let expression = document.getElementById('display').value;  
+    try {  
+        // Substituir 'mod' por '%' para usar na avaliação  
+        expression = expression.replace(/mod/g, '%');  
+        // Avaliar a expressão  
+        const result = eval(expression);  
+        document.getElementById('display').value = result;  
+    } catch (error) {  
+        document.getElementById('display').value = 'Erro';  
+    }  
+}  
+
+
